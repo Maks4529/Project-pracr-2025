3655cc543c3446f0fddc958711c02496c6fce8cb
Maks4529 <max555gos@gmail.com>
2025-05-24 12:35:38 +0300

    Add link to phone and update style for link

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -108,8 +108,10 @@ class Header extends React.Component {
         </div>
         <div className={styles.loginSignnUpHeaders}>
           <div className={styles.numberContainer}>
+            <a href="tel:8773553585">
             <img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone.png`} alt='phone' />
             <span>(877)&nbsp;355-3585</span>
+            </a>
           </div>
           <div className={styles.userButtonsContainer}>
             {this.renderLoginButtons()}
